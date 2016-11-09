b768cb9622e1f3424b0765ac9c6adf319919c9e3
Yossi Cohen <yossi.cohen@live.com>
2016-11-09 17:22:20 +0200

    ref

--- src/components/appbar/SearchDialog.js
+++ src/components/appbar/SearchDialog.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
-import AutoComplete from 'material-ui/AutoComplete';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+
+import Search from '../Search';
 
 const styles = {
     container: {
         textAlign: 'center',
         paddingTop: 200,
     },
-};
-
-//lilox:TODO - auto-complete data source (should come from server)
-const dataSource = [
-  {textKey: 'Material UI', valueKey: 'value-1'},
-  {textKey: 'Elemental UI', valueKey: 'value-2'},
-  {textKey: 'Grommet', valueKey: 'value-3'},
-  {textKey: 'Mui', valueKey: 'value-4'},
-  {textKey: 'Rebass', valueKey: 'value-5'},
-];
-
-const dataSourceConfig = {
-  text: 'textKey',
-  value: 'valueKey',
-};
-
-const focusNameInputField = ref => {
-    if (ref)
-        ref.focus();
 };
 
 export default class SearchDialog extends React.Component {
@@ -49,26 +31,17 @@ export default class SearchDialog extends React.Component {
             <div style={styles.container}>
                 <Dialog
                         ref="dialog"
-                        title="Search"
                         open={this.state.open}
                         actions={standardActions}
                         onRequestClose={this.handleCloseSearch.bind(this)}
                         >
-                        <AutoComplete
-                            ref={focusNameInputField}
-                            hintText="Type something ..."
-                            filter={AutoComplete.fuzzyFilter}
-                            dataSource={dataSource}
-                            dataSourceConfig={dataSourceConfig}
-                            maxSearchResults={5}
-                        />
+                        <Search />
                 </Dialog>
             </div>
         );
     }
 
     handleCloseSearch() {
-        console.log('Search: handleCloseSearch')
         this.setState({ open: false });
     };
 }
