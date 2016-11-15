761f81206ab5e432d3a66c40638739268dbc3b58
Yossi Cohen <yossi.cohen@live.com>
2016-11-15 17:59:36 +0200

    ref

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import "../style.css"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
+
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import store from "./store";
 
@@ -15,15 +21,17 @@ import Settings from './components/Settings/Settings';
 import Video from './components/Video/Video';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path="/" component={App}>
-                <IndexRoute component={Search} />
-                <Route path="/search" component={Search} />
-                <Route path="/video" component={Video} />
-                <Route path="/settings" component={Settings} />
-            </Route>
-        </Router> 
-    </Provider>,
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Provider store={store}>
+            <Router history={hashHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={Search} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/video" component={Video} />
+                    <Route path="/settings" component={Settings} />
+                </Route>
+            </Router>
+        </Provider>
+    </MuiThemeProvider>,
     document.getElementById('app')
 );
