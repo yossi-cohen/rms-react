6be99becfd6865f82f337bbb59325a343d37d460
Yossi Cohen <yossi.cohen@live.com>
2016-11-17 14:19:46 +0200

    fetch/axios

--- src/actions/videoActions.js
+++ src/actions/videoActions.js
@@ -4,10 +4,22 @@ import * as types from '../constants/ActionTypes'
 
 export function fetchVideos() {
   return function (dispatch) {
-    dispatch(fetchVideosRequest())
+    dispatch(fetchVideosRequest());
+
+    //lilox
+    let useAxios = false;
+    
+    if (useAxios) {
       return axios.get('http://localhost:3000/api/videos')
-      .then(res => dispatch(fetchVideosSuccess(res.data)))
-      .catch(ex => dispatch(fetchVideosFailure(ex)));
+        .then(res => { dispatch(fetchVideosSuccess(res.data)) })
+        .catch(ex => dispatch(fetchVideosFailure(ex)));
+    }
+    else {
+      return fetch('http://localhost:3000/api/videos')
+        .then((res) => res.json())
+        .then((json) => { dispatch(fetchVideosSuccess(json)) })
+        .catch(ex => dispatch(fetchVideosFailure(ex)));
+    }
   }
 }
 
