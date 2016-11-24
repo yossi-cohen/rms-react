bf7a2f1e398cee206a8610ab169d50770d90a6f6
yossico <yossi.cohen@live.com>
2016-11-24 21:29:47 +0200

    ref

--- src/components/Cesium/CesiumComponent.js
+++ src/components/Cesium/CesiumComponent.js
@@ -1,58 +1,99 @@
 import React from 'react';
 import Cesium from 'cesium/Source/Cesium.js';
+import Box from 'react-layout-components'
 import 'styles/cesium.css';
 import 'assets/cesiumWidgets.css'
 
-let cesiumViewerOptions = {
-  animation: false,
-  baseLayerPicker: false,
-  fullscreenButton: true,
-  fullscreenElement: 'cesiumDiv',
-  geocoder: true,
-  homeButton: true,
-  infoBox: true,
-  sceneModePicker: true,
-  selectionIndicator: true,
-  timeline: false,
-  navigationHelpButton: false,
-  navigationInstructionsInitiallyVisible: true,
-  automaticallyTrackDataSourceClocks: false
-};
-
-const styles = {
-  cesium: {
-    width: '640px',
-    height: '480px'
-  },
-}
-
 class CesiumComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
 
-  shouldComponentUpdate() {
-    return false;
-  }
+        this.state = {
+            clickPositions: []
+        }
 
-  componentDidMount() {
-    window.CESIUM_BASE_URL = '/cesium/';
-    Cesium.BingMapsApi.defaultKey = 'AlrnjpmA4KiONSspH1oyt38LOXi3FXPhf8Iy3jDyuzXnIv-DEMGGaiJdyikzFArD';
-    this.viewer = new Cesium.Viewer(this.refs.cesiumNode, cesiumViewerOptions);
-  }
-
-  componentWillUnmount() {
-    if (this.viewer) {
-      this.viewer.destroy();
+        // var click1 = new Cesium.Cartesian3(-2155350.2, -4622163.4, 3817393.1);
+        // this.state.clickPositions.push(click1);
     }
-  }
 
-  render() {
-    return (
-      <div ref="cesiumNode" id="cesiumDiv" className="cesium-widget">
-      </div>
-    );
-  }
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentDidMount() {
+        window.CESIUM_BASE_URL = '/cesium/';
+        Cesium.BingMapsApi.defaultKey = 'AlrnjpmA4KiONSspH1oyt38LOXi3FXPhf8Iy3jDyuzXnIv-DEMGGaiJdyikzFArD';
+        this.viewer = this.createCesiumViewer();
+        this.handleClick();
+    }
+
+    componentWillUnmount() {
+        if (this.viewer)
+            this.viewer.destroy();
+    }
+
+    render() {
+        return (
+            <Box width={500} height={300} justifyContent="center" alignItems="flex-start" alignSelf="center">
+                <div ref="cesiumNode" id="cesiumContainer" className="cesium-widget">
+                </div>
+            </Box>
+        );
+    }
+
+    createCesiumViewer() {
+        const cesiumViewerOptions = {
+            animation: false,
+            baseLayerPicker: false,
+            fullscreenButton: true,
+            fullscreenElement: 'cesiumContainer',
+            geocoder: true,
+            homeButton: true,
+            infoBox: true,
+            sceneModePicker: false,
+            selectionIndicator: true,
+            timeline: false,
+            navigationHelpButton: false,
+            navigationInstructionsInitiallyVisible: false,
+            automaticallyTrackDataSourceClocks: false
+        };
+
+        // home
+        const latitude = 32.80;
+        const longitude = 35.13;
+        const height = 17.0;
+
+        const west = longitude;
+        const south = latitude;
+        const east = longitude + 0.01;
+        const north = latitude + 0.01;
+        let extent = Cesium.Rectangle.fromDegrees(west, south, east, north);
+
+        Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
+        Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+
+        return new Cesium.Viewer(this.refs.cesiumNode, cesiumViewerOptions);
+    }
+
+    handleClick() {
+        // handle click
+        let viewer = this.viewer;
+        viewer.canvas.addEventListener('click', function (e) {
+            let mousePosition = new Cesium.Cartesian2(e.clientX, e.clientY);
+
+            let ellipsoid = viewer.scene.globe.ellipsoid;
+            let cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
+            if (cartesian) {
+                let cartographic = ellipsoid.cartesianToCartographic(cartesian);
+                let longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
+                let latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
+
+                console.log(longitudeString + ', ' + latitudeString);
+            } else {
+                console.log('Globe was not picked');
+            }
+        }, false);
+    }
 }
 
 export default CesiumComponent;
